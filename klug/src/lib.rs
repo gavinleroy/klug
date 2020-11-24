c43dc841db97b455b0f56b24b00e4b3b6321bd0c
gavinleroy <gavinleroy6@gmail.com>
2020-11-24 15:37:55 -0700

    Almost parsing simple expressions, update needed for Expr and Number/OP

--- klug/src/lib.rs
+++ klug/src/lib.rs
@@ -1,7 +1,85 @@
+mod utils;
+
+#[derive(Debug, PartialEq)]
+pub struct Expr {
+    pub lhs: Number,
+    pub rhs: Number,
+    pub op: Op,
+}
+
+impl Expr {
+    pub fn new(s: &str) -> Self {
+        Self {
+            lhs: Number(1),
+            rhs: Number(2),
+            op: Op::Plus
+        }
+    }
+}
+
+
+#[derive(Debug, PartialEq)]
+pub struct Number(i32);
+
+impl Number {
+    pub fn new(s: &str) -> Self {
+       Self(s.parse().unwrap()) 
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Op {
+    Plus,
+    Mult,
+    Sub,
+    Div,
+}
+
+impl Op {
+    pub fn new(s: &str) -> Self {
+        match s {
+            "+" => Self::Plus,
+            "*" => Self::Mult,
+            "-" => Self::Sub,
+            "/" => Self::Div,
+            _ => panic!("invalid operator"),
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn parse_number() {
+        assert_eq!(Number::new("123"), Number(123));
+    }
+    #[test]
+    fn parse_add() {
+        assert_eq!(Op::new("+"), Op::Plus);
+    }
+    #[test]
+    fn parse_mult() {
+        assert_eq!(Op::new("*"), Op::Mult);
+    }
+    #[test]
+    fn parse_sub() {
+        assert_eq!(Op::new("-"), Op::Sub);
+    }
+    #[test]
+    fn parse_div() {
+        assert_eq!(Op::new("/"), Op::Div);
+    }
+    #[test]
+    fn parse_expr1plus2() {
+        assert_eq!(
+            Expr::new("1+2"), 
+            Expr { 
+                lhs: Number(1), 
+                rhs: Number(2), 
+                op: Op::Plus, 
+            });
     }
 }
---
+++ klug/src/utils.rs
@@ -0,0 +1,50 @@
+pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
+        let digits_end = s 
+            .char_indices() 
+            .find_map(|(idx, c)| if c
+                      .is_ascii_digit() { None } else { Some(idx) }) 
+            .unwrap_or_else(|| s.len());
+
+        let digits = &s[..digits_end];
+        let remainder = &s[digits_end..];
+        (digits, remainder)
+}
+
+pub(crate) fn extract_op(s: &str) -> (&str, &str) {
+    match &s[0..1] {
+        "+" | "*" | "-" | "/" => {},
+        _ => panic!("no operator"),
+    }
+    (&s[0..1], &s[1..])
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn do_not_extract_anything_from_empty_input() {
+        assert_eq!(extract_digits(""), ("", ""));
+    }
+    #[test]
+    fn extract_digits_with_no_remainder() {
+        assert_eq!(extract_digits("100"), ("100", ""));
+    }
+    #[test]
+    fn extract_plus() {
+        assert_eq!(extract_op("+2"), ( "+", "2"));
+    }
+    #[test]
+    fn extract_minus() {
+        assert_eq!(extract_op("-10"), ("-", "10"));
+    }
+    #[test]
+    fn extract_star() {
+        assert_eq!(extract_op("*3"), ("*", "3"));
+    }
+    #[test]
+    fn extract_slash() {
+        assert_eq!(extract_op("/4"), ("/", "4"));
+    }
+}
