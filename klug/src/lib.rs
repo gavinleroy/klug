21dd210a537591f8fbf89db469e1705e886e65c5
gavinleroy <gavinleroy6@gmail.com>
2020-11-24 22:19:16 -0700

    More support for parsing

--- klug/src/lib.rs
+++ klug/src/lib.rs
@@ -8,22 +8,21 @@ pub struct Expr {
 }
 
 impl Expr {
-    pub fn new(s: &str) -> Self {
-        Self {
-            lhs: Number(1),
-            rhs: Number(2),
-            op: Op::Plus
-        }
+    pub fn new(s: &str) -> (&str, Self) {
+        let (s, lhs) = Number::new(s);
+        let (s, op) = Op::new(s);
+        let (s, rhs) = Number::new(s);
+        (s, Self { lhs, rhs, op })
     }
 }
-
 
 #[derive(Debug, PartialEq)]
 pub struct Number(i32);
 
 impl Number {
-    pub fn new(s: &str) -> Self {
-       Self(s.parse().unwrap()) 
+    pub fn new(s: &str) -> (&str, Self) {
+        let (num, s) = utils::extract_digits(s);
+        (s, Self(num.parse().unwrap())) 
     }
 }
 
@@ -36,14 +35,16 @@ pub enum Op {
 }
 
 impl Op {
-    pub fn new(s: &str) -> Self {
-        match s {
+    pub fn new(s: &str) -> (&str, Self) {
+        let (op, s) = utils::extract_op(s);
+        let op = match op {
             "+" => Self::Plus,
             "*" => Self::Mult,
             "-" => Self::Sub,
             "/" => Self::Div,
             _ => panic!("invalid operator"),
-        }
+        };
+        (s, op)
     }
 }
 
@@ -54,32 +55,27 @@ mod tests {
 
     #[test]
     fn parse_number() {
-        assert_eq!(Number::new("123"), Number(123));
+        assert_eq!(Number::new("123"), ("", Number(123)));
     }
     #[test]
     fn parse_add() {
-        assert_eq!(Op::new("+"), Op::Plus);
+        assert_eq!(Op::new("+"), ("", Op::Plus));
     }
     #[test]
     fn parse_mult() {
-        assert_eq!(Op::new("*"), Op::Mult);
+        assert_eq!(Op::new("*"), ("", Op::Mult));
     }
     #[test]
     fn parse_sub() {
-        assert_eq!(Op::new("-"), Op::Sub);
+        assert_eq!(Op::new("-"), ("", Op::Sub));
     }
     #[test]
     fn parse_div() {
-        assert_eq!(Op::new("/"), Op::Div);
+        assert_eq!(Op::new("/"), ("", Op::Div));
     }
     #[test]
     fn parse_expr1plus2() {
-        assert_eq!(
-            Expr::new("1+2"), 
-            Expr { 
-                lhs: Number(1), 
-                rhs: Number(2), 
-                op: Op::Plus, 
-            });
+        assert_eq!(Expr::new("1+2"), 
+                   ("", Expr { lhs: Number(1), rhs: Number(2), op: Op::Plus, }));
     }
 }
