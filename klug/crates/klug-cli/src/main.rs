f57bfd24a330f20bd04e0569195f902c7c39ccc2
gavinleroy <gavinleroy6@gmail.com>
2020-12-05 21:50:35 -0700

    REPL, updated

--- klug/crates/klug-cli/src/main.rs
+++ klug/crates/klug-cli/src/main.rs
@@ -1,38 +1,22 @@
+use klug::parser::Parser;
 use std::io::{self, Write};
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let mut stdout = io::stdout();
-    let mut stderr = io::stderr();
 
     let mut input = String::new();
-    let mut env = klug::env::Env::default();
 
     loop {
-        write!(stdout, "klug> ");
+//        write!(stdout, "klug → ")?;
+        write!(stdout, "klug \u{1F449} ")?;
         stdout.flush()?;
 
         stdin.read_line(&mut input)?;
 
-        match run(input.trim(), &mut env) {
-            Ok(Some(val)) => writeln!(stdout, "{}", val)?,
-            Ok(None) => { }
-            Err(msg) => writeln!(stderr, "{}", msg)?,
-        }
+        let parse = Parser::new(&input).parse();
+        println!("{}", parse.debug_tree());
+
         input.clear();
-    }
-}
-
-fn run(s: &str, env: &mut klug::Env) -> Result<Option<klug::Value>, String> {
-    let parse = klug::parse(s)
-        .map_err(|msg| format!("ERR! {}", msg))?;
-    let ev = parse
-        .eval(env)
-        .map_err(|msg| format!("ERR! {}", msg))?;
-
-    if ev == klug::Value::Unit {
-        Ok(None)
-    } else {
-        Ok(Some(ev))
     }
 }
